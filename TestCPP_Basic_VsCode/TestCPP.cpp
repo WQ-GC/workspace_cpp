561e829df27f5da0effdc39064c35e6bf93a43b6
WQ <quah.sa.wilson@gmail.com>
2021-07-06 16:03:15 +0800

    Basic VSCode Project

---
+++ TestCPP_Basic_VsCode/TestCPP.cpp
@@ -0,0 +1,17 @@
+//Test CPP
+
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+
+  cout << "Hello World" << endl;
+
+//Conditional Compilation  
+#ifdef WILSON_MODE
+  cout << "WILSON_MODE" << endl;
+#endif  
+
+  return 0;
+}
